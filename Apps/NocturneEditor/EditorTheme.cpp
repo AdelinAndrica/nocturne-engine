efd91b83565ea170514271d65ee281c85e3aa01a
AdelinAndrica <adelinandrica@gmail.com>
2026-09-16 16:37:54 +0300

    phase13: reduce UI contrast for minimalist polish

--- Apps/NocturneEditor/EditorTheme.cpp
+++ Apps/NocturneEditor/EditorTheme.cpp
@@ -5,22 +5,22 @@ namespace nocturne::editor
     namespace
     {
         const ThemeColors kColors{
-            RGB(11, 17, 24),    // windowBg
-            RGB(17, 25, 35),    // panelBg
-            RGB(21, 31, 43),    // panelBgAlt
-            RGB(15, 25, 37),    // viewportBg
-            RGB(15, 23, 33),    // toolbarBg
-            RGB(13, 21, 31),    // inputBg
-            RGB(24, 35, 51),    // buttonBg
-            RGB(32, 48, 70),    // buttonHover
-            RGB(39, 54, 73),    // border
-            RGB(231, 238, 247), // textPrimary
-            RGB(143, 161, 183), // textMuted
-            RGB(22, 138, 251),  // accent
-            RGB(46, 155, 255),  // accentHover
-            RGB(77, 213, 106),  // success
-            RGB(246, 184, 74),  // warning
-            RGB(255, 92, 108)   // danger
+            RGB(10, 15, 22),    // windowBg
+            RGB(16, 23, 32),    // panelBg
+            RGB(20, 28, 39),    // panelBgAlt
+            RGB(13, 22, 33),    // viewportBg
+            RGB(14, 21, 30),    // toolbarBg
+            RGB(12, 19, 28),    // inputBg
+            RGB(21, 31, 45),    // buttonBg
+            RGB(27, 40, 58),    // buttonHover
+            RGB(34, 47, 63),    // border
+            RGB(222, 230, 240), // textPrimary
+            RGB(132, 149, 171), // textMuted
+            RGB(25, 132, 236),  // accent
+            RGB(42, 148, 250),  // accentHover
+            RGB(78, 205, 112),  // success
+            RGB(226, 166, 67),  // warning
+            RGB(232, 86, 103)   // danger
         };
 
         const ThemeMetrics kMetrics{};
